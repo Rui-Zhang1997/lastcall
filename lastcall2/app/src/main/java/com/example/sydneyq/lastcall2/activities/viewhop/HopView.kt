e67920c204ab133aa3a89ef5df6116350ebe2e39
Rui Zhang <r.zhang194@gmail.com>
2018-09-30 07:55:51 -0400

    Close to done

--- lastcall2/app/src/main/java/com/example/sydneyq/lastcall2/activities/viewhop/HopView.kt
+++ lastcall2/app/src/main/java/com/example/sydneyq/lastcall2/activities/viewhop/HopView.kt
@@ -2,24 +2,44 @@ package com.example.sydneyq.lastcall2.activities.viewhop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ListFragment
 import android.util.Log
 import android.view.Gravity
 import android.view.MotionEvent
 import com.example.sydneyq.lastcall2.R
+import com.example.sydneyq.lastcall2.api.models.HopMember
+import com.example.sydneyq.lastcall2.api.rest.lastCallREST
+import com.example.sydneyq.lastcall2.fragments.SlideUpListFragment
 import com.mancj.slideup.SlideUp
 import com.mancj.slideup.SlideUpBuilder
 import com.mapbox.mapboxsdk.Mapbox
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.AsyncSubject
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_hop_view.*
 
 class HopView : AppCompatActivity() {
-    val slideUpDisposable = AsyncSubject.create<Float>()
-    val slideDownLimit = 100f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hop_view)
         Mapbox.getInstance(this, "pk.eyJ1IjoicnoxODciLCJhIjoiY2o3dXZpZzR1NGJwNzJ3bzZ4cGo1b3cxMiJ9.e9NJyxQ8IXckHfWPPAhlOQ");
         mapView.onCreate(savedInstanceState)
+        val args = intent.extras.getBundle("args")
+        val member = args.getSerializable("member") as HopMember
+        lastCallREST.getHop(member.currentHop)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map { hop ->
+                    val listFragment = SlideUpListFragment()
+                    val args = Bundle()
+                    args.putSerializable("bars", hop.bars)
+                    args.putInt("type", 0)
+                    listFragment.arguments = args
+                    val fragmentManager = this.supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.replace(R.id.slide_up_fragment, listFragment)
+                    transaction.addToBackStack(null)
+                }
     }
 }
--- lastcall2/app/src/main/java/com/example/sydneyq/lastcall2/fragments/ListFragment.kt
+++ lastcall2/app/src/main/java/com/example/sydneyq/lastcall2/fragments/ListFragment.kt
@@ -3,28 +3,116 @@ package com.example.sydneyq.lastcall2.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.sydneyq.lastcall2.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.sydneyq.lastcall2.api.models.BarData
+import com.example.sydneyq.lastcall2.api.models.DrinkData
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.bar_list_item.view.*
+import kotlinx.android.synthetic.main.drink_list_item.view.*
+import kotlinx.android.synthetic.main.fragment_list.*
+import org.apache.commons.lang3.StringUtils
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class BarListFragment : Fragment() {
+class BarViewHolder(val v: View) : RecyclerView.ViewHolder(v) {
+    fun bind(data: BarData, listener: (BarData) -> Unit) = with(itemView) {
+        bar_name.text = data.barName
+        bar_address.text = data.barAddress
+        bar_rating.text = data.barRating
+        bar_cost.text = data.barCost.toString()
+        setOnClickListener { listener(data) }
+    }
+}
 
+class BarListAdapter(val data: Array<BarData>, val sub: PublishSubject<BarData>) : RecyclerView.Adapter<BarViewHolder>() {
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position % 2;
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BarViewHolder {
+        if (viewType == 0) {
+            return BarViewHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.bar_list_item, parent, false) as View)
+        } else {
+            return BarViewHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.bar_list_item_red, parent, false) as View)
+        }
+    }
+
+    override fun onBindViewHolder(holder: BarViewHolder, position: Int) {
+        holder.bind(data.get(position), { data ->
+            sub.onNext(data)
+        })
+    }
+}
+
+class DrinkViewHolder(val v: View) : RecyclerView.ViewHolder(v) {
+    fun bind(data: DrinkData, listener: (BarData) -> Unit) = with(itemView) {
+        drink_price.text = data.drinkCost.toString()
+        drink_title.text = data.drinkName
+        drink_alcohol.text = data.drinkName
+        drink_ingredients.text = StringUtils.join(data.incredients, ",")
+    }
+}
+
+class DrinkListAdapter(val data: Array<DrinkData>) : RecyclerView.Adapter<DrinkViewHolder>() {
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position % 2;
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DrinkViewHolder {
+        if (viewType == 0) {
+            return DrinkViewHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.drink_list_item, parent, false) as View)
+        } else {
+            return DrinkViewHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.drink_list_item_red, parent, false) as View)
+        }
+    }
+
+    override fun onBindViewHolder(holder: DrinkViewHolder, position: Int) {
+        holder.bind(data.get(position), { data -> null })
+    }
+}
+
+class SlideUpListFragment : Fragment() {
+    val barSelectedSubject = PublishSubject.create<BarData>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+        val args = arguments
+        bar_list_recyclerview.layoutManager = LinearLayoutManager(context)
+        if (args!!.getInt("type") == 0) {
+            bar_list_recyclerview.adapter = BarListAdapter(args!!.get("data") as Array<BarData>, barSelectedSubject)
+        } else {
+            bar_list_recyclerview.adapter = DrinkListAdapter(args!!.get("data") as Array<DrinkData>)
+        }
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-
+    fun displayDrinksList(bar: BarData) {
+        val transaction = fragmentManager!!.beginTransaction()
+        val drinksFragment = SlideUpListFragment()
+        val args = Bundle()
+        args.putInt("type", 1)
+        args.putSerializable("data", bar.menu)
+        drinksFragment.arguments = args
+        transaction.replace(R.id.bar_list_recyclerview, SlideUpListFragment())
+        transaction.addToBackStack(null)
+    }
 }
